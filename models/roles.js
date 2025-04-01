a9795c3ef0d172e957b13a114b7539c15ad3c695
thien-ui <148088738+thien-ui@users.noreply.github.com>
2025-04-01 15:49:49 +0700

    oke

---
+++ models/roles.js
@@ -0,0 +1,17 @@
+let mongoose = require('mongoose');
+
+let roleSchema = mongoose.Schema({
+    roleName:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    description:{
+        type:String,
+        default:""
+    }
+},{
+    timestamps:true
+})
+module.exports =  mongoose.model('role',roleSchema )
+   
